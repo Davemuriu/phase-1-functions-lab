0d03f15b60c6e99fe3882f3a7fcd7135038da3f0
David <dmuriu0@gmail.com>
2025-06-21 15:35:26 +0300

    Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+
+    if (distanceInFeet > 2500) {
+        return "does not allow rides over 2500 feet";
+    } else if (distanceInFeet <= 400) {
+        return 0;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    }
+}
